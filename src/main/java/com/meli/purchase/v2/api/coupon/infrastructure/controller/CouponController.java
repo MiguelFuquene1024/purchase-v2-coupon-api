be2088c4673cb97f2e2154e768917db996a0d6df
MiguelFuquene1024 <migueplay_10@hotmail.com>
2024-12-30 11:43:24 -0500

    updated

--- src/main/java/com/meli/purchase/v2/api/coupon/infrastructure/controller/CouponController.java
+++ src/main/java/com/meli/purchase/v2/api/coupon/infrastructure/controller/CouponController.java
@@ -3,6 +3,7 @@ package com.meli.purchase.v2.api.coupon.infrastructure.controller;
 import com.meli.purchase.v2.api.coupon.application.service.CouponService;
 import com.meli.purchase.v2.api.coupon.infrastructure.adapter.CouponServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -17,6 +18,10 @@ public class CouponController {
     @GetMapping("/top5")
     public Mono<Map<String,Integer>> getTop5Coupon() {
         return couponService.getTop5Coupons();
+    }
+    @GetMapping
+    public Mono<ResponseEntity<String>> getHealthCheck() {
+        return Mono.just(ResponseEntity.ok("health check ok"));
     }
 
 }
